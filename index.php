9895a2dbd2d90af6150c95787e95821bed1fc0aa
SergiuszOlszewski <sergiusz@mail.com>
2018-10-19 10:40:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php if(trim($_GET["firstname"])&&trim($_GET["lastname"]))
-	file_put_contents("list", $_GET["firstname"].",".$_GET["lastname"]."\n", FILE_APPEND);
-$people=file('list', FILE_IGNORE_NEW_LINES);
+	file_put_contents("list", $_GET["firstname"].",".$_GET["lastname"]."\n", FILE_APPEND) or die("nie można zapisać do pliku");
+$people=file('list', FILE_IGNORE_NEW_LINES) or die("nie można otworzyć pliku");
 foreach($people as $person)
 	echo (explode(",", $person)[0].".".explode(",", $person)[1].$occurrenceNum[explode(",", $person)[0].",".explode(",", $person)[1]]++."@company.com<br>\n"); ?>
