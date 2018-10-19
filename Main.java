1ceccf22c0fe95bec4d5e13d9e7cef223eda5ab6
SergiuszOlszewski <sergiusz@mail.com>
2018-10-19 10:43:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.Scanner;
 
 /*
+Kod w PHP:
 <?php if(trim($_GET["firstname"])&&trim($_GET["lastname"]))
 	file_put_contents("list", $_GET["firstname"].",".$_GET["lastname"]."\n", FILE_APPEND) or die("nie można zapisać do pliku");
 $people=file('list', FILE_IGNORE_NEW_LINES) or die("nie można otworzyć pliku");
 foreach($people as $person)
 	echo (explode(",", $person)[0].".".explode(",", $person)[1].$occurrenceNum[explode(",", $person)[0].",".explode(",", $person)[1]]++."@company.com<br>\n"); ?>
-	5 linijek z zapisem do pliku i sprawdzaniem poprawności
+5 linijek z zapisem do pliku i sprawdzaniem poprawności
  */
 
 public class Main
